26bc9a61642165c1a99c5ee2e27a2b4344a81752
vansh comar <vanshcomar1@gmail.com>
2022-05-31 23:40:37 +0530

    Uploading coursse code

---
+++ Day 1 Project Band Name Generator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator")
+print("What's the name of the city you grew up in?")
+city_name = input()
+print("What's you pet's name?")
+pet_name = input()
+print("Your Band name could be",city_name,pet_name)
---
+++ Day 2 Project Tip Calculator/main.py
@@ -0,0 +1,5 @@
+print("Welcome to the tip calculator.")
+total_bill = float(input("What was the total bill? $"))
+tip_percentage = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+number_of_people = int(input("How many people to split the bill? "))
+print(f"Each person should pay: ${round((1+tip_percentage/100) * total_bill / number_of_people, 2)}")
